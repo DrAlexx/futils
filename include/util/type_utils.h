ae3a1c66b1aac4f6f362d1ea283e4188e836c0b1
Olexiy Khokhlov <DrAlexx@users.noreply.github.com>
2021-04-19 12:20:40 +0300

    Add std::reference_wrapper utils

--- include/util/type_utils.h
+++ include/util/type_utils.h
@@ -17,3 +17,22 @@ using shrink_to_minimal_unsigned = std::conditional<N <= std::numeric_limits<uin
                                    typename std::conditional<N <= std::numeric_limits<uint32_t>::max(), uint32_t,
                                    uint64_t>::type>::type>;
 
+template<typename X>
+struct is_reference_wrapper : std::false_type {
+    using value_type = X;
+};
+ 
+template<typename X>
+struct is_reference_wrapper<std::reference_wrapper<X>> : std::true_type {
+    using value_type = typename std::reference_wrapper<X>::type;
+};
+ 
+template <typename X>
+constexpr typename std::reference_wrapper<X>::type& get_reference(std::reference_wrapper<X> data) {
+    return data.get();
+}
+
+template <typename X>
+constexpr X& get_reference(X& data) {
+    return data;
+}
